ae59d700ea576c00fd0bba00b0687c787ac5aa19
karl <elrion018@gmail.com>
2023-03-12 20:39:00 +0900

    feature : 클라이언트 코드 작성

--- patterns/creationals/Shape/index.ts
+++ patterns/creationals/Shape/index.ts
@@ -1,3 +1,21 @@
+import { Circle } from './Circle';
+import { Rectangle } from './Rectangle';
+
 /**
  * 프로토타입(Prototype) 패턴의 예제로서 Shape, Rectangle, Circle 모듈 구현
  */
+const circle = new Circle();
+
+circle.setPosition({
+  xPosition: 2,
+  yPosition: 10,
+});
+circle.setRadius(5);
+
+const copiedCircle = circle.clone();
+const rectangle = new Rectangle();
+
+rectangle.setHeight(10);
+rectangle.setWidth(10);
+
+const copied = rectangle.clone();
