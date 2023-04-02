e7d15302611e84b2d00e6895a43aceb5210e5132
karl <elrion018@gmail.com>
2023-04-02 20:01:32 +0900

    fix : 목업된 비디오파일을 받도록 변경

--- patterns/structurals/proxy/videoManager/Video.ts
+++ patterns/structurals/proxy/videoManager/Video.ts
@@ -8,21 +8,29 @@ interface 비디오생성자매개변수 {
   확장자: 확장자종류;
 }
 
+export class 실제비디오파일 {
+  // 여러가지 정의들 ..
+}
+
 export class 비디오 {
-  private 아이디: string;
-  private 이름: string;
-  private 재생시간: number;
-  private 크기: number;
-  private 확장자: 확장자종류;
+  private 아이디: string | null;
+  private 이름: string | null;
+  private 재생시간: number | null;
+  private 크기: number | null;
+  private 확장자: 확장자종류 | null;
 
-  constructor({ 아이디, 이름, 재생시간, 크기, 확장자 }: 비디오생성자매개변수) {
-    // 원래 실제 파일을 매개변수로 받으면 좋으나, 예제이므로 대체
+  constructor(비디오파일: 실제비디오파일) {
+    this.아이디 = null;
+    this.이름 = null;
+    this.재생시간 = null;
+    this.크기 = null;
+    this.확장자 = null;
 
-    this.아이디 = 아이디;
-    this.이름 = 이름;
-    this.재생시간 = 재생시간;
-    this.크기 = 크기;
-    this.확장자 = 확장자;
+    this.비디오정보파싱하기(비디오파일);
+  }
+
+  비디오정보파싱하기(비디오파일: 실제비디오파일) {
+    // 아이디, 이름, 재생시간, 크기, 확장자 정보를 실제 파일에서 파싱 ...
   }
 
   비디오정보얻기() {
