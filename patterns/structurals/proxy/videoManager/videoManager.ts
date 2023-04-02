b2eff81f1611b20be733d3184894450d79a2dde8
karl <elrion018@gmail.com>
2023-04-02 20:06:26 +0900

    feature : 비디오관리자 인터페이스 정의

--- patterns/structurals/proxy/videoManager/videoManager.ts
+++
@@ -1,5 +0,0 @@
-import { 비디오 } from './Video';
-
-interface 비디오관리자 {
-  비디오목록얻기: () => 비디오[];
-}
--- patterns/structurals/proxy/videoManager/Video.ts
+++ patterns/structurals/proxy/videoManager/Video.ts
@@ -1,11 +1,11 @@
 type 확장자종류 = 'mp4' | 'avi' | 'wmv'; // 기타 다른 확장자들도 많으나 이 3개 까지..
 
-interface 비디오생성자매개변수 {
-  아이디: string;
-  이름: string;
-  재생시간: number;
-  크기: number;
-  확장자: 확장자종류;
+export interface 비디오정보 {
+  아이디: string | null;
+  이름: string | null;
+  재생시간: number | null;
+  크기: number | null;
+  확장자: 확장자종류 | null;
 }
 
 export class 실제비디오파일 {
@@ -33,7 +33,7 @@ export class 비디오 {
     // 아이디, 이름, 재생시간, 크기, 확장자 정보를 실제 파일에서 파싱 ...
   }
 
-  비디오정보얻기() {
+  비디오정보얻기(): 비디오정보 {
     const { 아이디, 이름, 재생시간, 확장자, 크기 } = this;
 
     return { 아이디, 이름, 재생시간, 확장자, 크기 };
